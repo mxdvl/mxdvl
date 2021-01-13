474e84bdf00552793fc775d63ee1e6f000752424
Max Duval <max.duval@guardian.co.uk>
2021-01-13 16:58:58 -0500

    add header and footer

---
+++ src/components/Header.svelte
@@ -0,0 +1,41 @@
+<header class="header wrap wide" role="banner">
+  <nav>
+    <ul>
+      <li class="home  active">
+        <a class="branding" href="https://www.mxdvl.com" rel="home">
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 36 36"
+            style="height: 36px"
+            xml:space="preserve">
+            <g id="cmps" fill="none" stroke="" stroke-width="">
+              <line x1="18" y1="0" x2="18" y2="36" />
+              <line x1="18" y1="18" x2="36" y2="18" />
+              <circle cx="18" cy="18" r="12" /></g
+            >
+          </svg>
+          <span>MXDVL</span>
+        </a>
+      </li>
+
+      <li class="menu-item ">
+        <a href="https://www.mxdvl.com/works">WORKS</a>
+      </li>
+
+      <li class="menu-item ">
+        <a href="https://www.mxdvl.com/profile">PROFILE</a>
+      </li>
+
+      <!--
+                                <li class="menu-item">
+          <a href="https://www.mxdvl.com/fr">
+            Fran&ccedil;ais 🥐          </a>
+        </li>
+                -->
+    </ul>
+  </nav>
+</header>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,67 @@
 <script>
-	import Counter from '$components/Counter.svelte';
+  import Counter from "$components/Counter.svelte";
+  import Header from "$components/Header.svelte";
+  import Footer from "$components/Header.svelte";
 </script>
 
-<main>
-	<h1>Hello world!</h1>
+<Header />
 
-	<Counter/>
-	<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
+<Footer />
 
 <style>
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-	}
+  :root {
+    font-family: "IBM Plex Variable", -apple-system, BlinkMacSystemFont,
+      "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
+      "Helvetica Neue", sans-serif;
 
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
+    --accent: hsl-str(hsl(24, 96%, 60%));
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
+    --lightest: hsl-str(hsl(240, 12%, 100%));
+    --light: hsl-str(hsl(204, 18%, 72%));
 
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
+    --dark: hsl-str(hsl(240, 12%, 18%)); /*rgb(60, 72, 84)*/
+    --darkest: hsl-str(hsl(204, 24%, 18%));
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
+  :root.dark {
+    --darkest: hsl-str(hsl(204, 24%, 6%));
+    --dark: hsl-str(hsl(204, 24%, 18%));
 
-		p {
-			max-width: none;
-		}
-	}
+    --lightest: hsl-str(hsl(240, 24%, 96%));
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+
+    background-color: var(--dark);
+    color: var(--lightest);
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 4rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 2rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
 </style>
