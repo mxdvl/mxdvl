e6c48e11794ed6c4e7166e15a011583a61f7bfdf
Max Duval <max.duval@theguardian.com>
2021-12-09 21:01:19 +0000

    add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +9,7 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: "#svelte",
 		hostHeader: "X-Forwarded-Host"
 	}
