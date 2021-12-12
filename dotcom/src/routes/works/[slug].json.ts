61e9b1668a67934e87edbd7211cd9febfc5b9cf5
Max <hi@mxdvl.com>
2021-12-12 11:15:15 +0000

    set default formatter for typescript

--- dotcom/src/routes/works/[slug].json.ts
+++ dotcom/src/routes/works/[slug].json.ts
@@ -2,14 +2,12 @@ import type { RequestHandler } from "@sveltejs/kit";
 import { getWorks } from "../works.json";
 
 export const get: RequestHandler = async ({ params }) => {
-	const {slug} = params;
-	const work = getWorks().find(work => work.slug === slug);
+	const { slug } = params;
+	const work = getWorks().find((work) => work.slug === slug);
 
 	if (!work) return;
 
 	return {
-		body: {
-			work,
-		}
-	}
-}
+		body: { work }
+	};
+};
