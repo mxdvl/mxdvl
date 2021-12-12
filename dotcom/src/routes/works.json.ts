980bf333dbfb3f88c54972f77f21f3152613b9cd
Max <hi@mxdvl.com>
2021-12-12 15:58:54 +0000

    lint works
    with trailing comma

--- dotcom/src/routes/works.json.ts
+++ dotcom/src/routes/works.json.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
-const imports = import.meta.globEager('../../../content/works/**/*.en.md')
+const imports = import.meta.globEager("../../../content/works/**/*.en.md");
 
 export type Work = {
 	url: `https://www.mxdvl.com/works/${string}`;
@@ -8,31 +8,33 @@ export type Work = {
 	metadata: {
 		title: string;
 		date: `${number}-${number}` | string;
-	}
-	content: {
-		html: string
 	};
-}
+	content: {
+		html: string;
+	};
+};
 
-const isString = (value): value is string => typeof value === 'string' && value !== ''
+const isString = (value): value is string => typeof value === "string" && value !== "";
 
-export const getWorks = (): Work[] => Object.entries(imports).map(([path, entry]) => {
-	const slug = path.split('/').filter(Boolean).slice(-2)[0]
-	const parsed: Record<keyof Work['metadata'], unknown> = entry.metadata;
-	const metadata: Work['metadata'] = {
-		title: isString(parsed.title) ? parsed.title : "⚠️ MISSING TITLE",
-		date: isString(parsed.date) ? parsed.date : "MISSING DATE",
-	}
+export const getWorks = (): Work[] =>
+	Object.entries(imports).map(([path, entry]) => {
+		const slug = path.split("/").filter(Boolean).slice(-2)[0];
+		const parsed: Record<keyof Work["metadata"], unknown> = entry.metadata;
+		const metadata: Work["metadata"] = {
+			title: isString(parsed.title) ? parsed.title : "⚠️ MISSING TITLE",
+			date: isString(parsed.date) ? parsed.date : "MISSING DATE",
+		};
 
 		const url: Work["url"] = `https://www.mxdvl.com/works/${slug}`;
+		const content: Work["content"] = entry.default.render();
 
-	return {
+		return {
 			url,
-		slug,
-		metadata,
-		content,
-	}
-})
+			slug,
+			metadata,
+			content,
+		};
+	});
 
 export const get: RequestHandler = async ({ params }) => {
 	const works = getWorks();
@@ -40,6 +42,6 @@ export const get: RequestHandler = async ({ params }) => {
 	return {
 		body: {
 			works,
-		}
-	}
-}
+		},
+	};
+};
