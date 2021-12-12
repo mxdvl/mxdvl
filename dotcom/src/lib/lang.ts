e746f1168d21739f56312ef4195fa5c5e467eddc
Max <hi@mxdvl.com>
2021-12-12 19:03:59 +0000

    more robust language selection

--- dotcom/src/lib/lang.ts
+++ dotcom/src/lib/lang.ts
@@ -2,4 +2,4 @@ export type Lang = "en" | "fr";
 
 const frenchPages = ["/allo", "/profil", "/travaux"];
 
-export const pathLang = (path: string): Lang => frenchPages.includes(path) ? "fr" : "en"
+export const pathLang = (path: string): Lang => (frenchPages.find((page) => path.startsWith(page)) ? "fr" : "en");
