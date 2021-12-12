760a901d4444716c6b6a36468620d0f402df829b
Max <hi@mxdvl.com>
2021-12-12 15:57:44 +0000

    add theming support

--- dotcom/src/routes/__layout.svelte
+++ dotcom/src/routes/__layout.svelte
@@ -17,6 +17,27 @@
 	import Footer from "$lib/Footer.svelte";
 
 	import "../app.css";
+	import "../ibm-plex-var.css";
+
+	const setTheme = (theme: "light" | "dark" | "default") => {
+		const { classList } = document.querySelector("html");
+		classList.remove("light", "dark", "default");
+		switch (theme) {
+			case "light":
+				classList.add("light");
+				localStorage.setItem("theme", "light");
+				break;
+			case "dark":
+				classList.add("dark");
+				localStorage.setItem("theme", "dark");
+				break;
+			case "default":
+				classList.add("default");
+				localStorage.removeItem("theme");
+				break;
+		}
+		return theme;
+	};
 
 	export let lang: Lang;
 </script>
@@ -27,11 +48,9 @@
 	<slot />
 </main>
 
-<button
-	on:click={() => {
-		["light", "dark"].forEach((token) => window.document.body.classList.toggle(token));
-	}}>switch</button
->
+<button on:click={() => setTheme("light")}>light</button>
+<button on:click={() => setTheme("dark")}>dark</button>
+<button on:click={() => setTheme("default")}>reset</button>
 
 <Footer {lang} />
 
