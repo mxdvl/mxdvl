dac852d7f9f6b3fc2865d230352bebde04e5f3a1
Max <hi@mxdvl.com>
2021-12-13 01:02:20 +0000

    duplicate code for travaux
    symlinks break vite

---
+++ dotcom/src/routes/travaux/index.svelte
@@ -0,0 +1,31 @@
+<script context="module" lang="ts">
+	import type { Load } from "@sveltejs/kit";
+	import { Lang, pathLang } from "$lib/lang";
+	import type { Work } from "$lib/works";
+
+	export const load: Load = async ({ page, fetch }) => {
+		const url = "/works.json";
+		const res = await fetch(url);
+		const works = (await res.json()).works;
+
+		const lang: Lang = pathLang(page.path);
+
+		return {
+			props: {
+				works,
+				lang,
+			},
+		};
+	};
+</script>
+
+<script lang="ts">
+	import Works from "$lib/Works.svelte";
+
+	export let works: Work[];
+	export let lang: Lang;
+</script>
+
+## worky works
+
+<Works {lang} {works} />
