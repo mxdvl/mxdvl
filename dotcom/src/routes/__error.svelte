5102ef9b34e171286e563df23950313b9e1b887e
Max <hi@mxdvl.com>
2021-12-22 15:50:11 +0000

    add error page

---
+++ dotcom/src/routes/__error.svelte
@@ -0,0 +1,9 @@
+<section lang="en">
+	<h3>Not Found</h3>
+	<p>Nothing to see here, sadly :(</p>
+</section>
+
+<section lang="fr">
+	<h3>Perdu</h3>
+	<p>Rien à voir ici, malheureusement :(</p>
+</section>
