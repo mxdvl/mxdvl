c3910eab21988e86fff78f6c5e56fb096f606a4f
Max Duval <hi@mxdvl.com>
2021-12-25 21:24:15 +0000

    once more

--- dotcom/src/routes/works/[slug].json.ts
+++ dotcom/src/routes/works/[slug].json.ts
@@ -6,7 +6,7 @@ export const get: RequestHandler = async ({ params, path }) => {
 	const { slug } = params;
 	const lang: Lang = pathLang(path);
 	const work =
-		(await works).find((work) => work.urls[lang]?.endsWith(slug)) ?? works.find((work) => work.urls.en.endsWith(slug));
+		(await works).find((work) => work.urls[lang]?.endsWith(slug)) ?? (await works).find((work) => work.urls.en.endsWith(slug));
 
 	if (!work) return;
 
