a75aaf814fbc856dcf9b44bdba810ba8095163fb
Max <hi@mxdvl.com>
2021-12-28 23:00:25 +0000

    add profile en + fr

--- dotcom/src/routes/profil.svelte
+++ dotcom/src/routes/profil.svelte
@@ -1,1 +1,5 @@
-<h1>Mon profil, jeune padawan</h1>
+<h1>Le profil de Max</h1>
+
+<p>
+	En tant qu’ingénieur logiciel et designer, je bâti des modèles complexes à l’aide de médiums numériques et tangibles. Constamment en train d’esquisser une idée, tant avec un stylo qu’un écran, je cherche de nouvelles façons de créer des choses inspirantes et interactives. Mon travail est ancré dans une fascination pour les motifs, la géométrie et la simplicité.
+</p>
--- dotcom/src/routes/profile.svelte
+++ dotcom/src/routes/profile.svelte
@@ -1,1 +1,5 @@
-<h1>my profile, yo</h1>
+<h1>Max’s profile</h1>
+
+<p>
+	As a software engineer and designer, I transform complex ideas into refined products. Working with both client and user in mind, I create robust systems that are fast and accessible. Understanding what makes the world tick and improving workflows has lead me to work across cultures and industries, including: the UK, Japan & Canada, designing for non-profits, bespoke projects, hospitality, government bodies and property developers. Have a look at my work.
+</p>
