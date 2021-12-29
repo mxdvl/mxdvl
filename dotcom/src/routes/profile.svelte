64be19e23456bcc6e8da40bd49e75312209bc1d8
Max <hi@mxdvl.com>
2021-12-29 09:11:19 +0000

    add cv to profile

--- dotcom/src/routes/profile.svelte
+++ dotcom/src/routes/profile.svelte
@@ -1,5 +1,125 @@
 <h1>Max’s profile</h1>
 
 <p>
-	As a software engineer and designer, I transform complex ideas into refined products. Working with both client and user in mind, I create robust systems that are fast and accessible. Understanding what makes the world tick and improving workflows has lead me to work across cultures and industries, including: the UK, Japan & Canada, designing for non-profits, bespoke projects, hospitality, government bodies and property developers. Have a look at my work.
+	As a software engineer and designer, I transform complex ideas into refined products. Working with both client and
+	user in mind, I create robust systems that are fast and accessible. Understanding what makes the world tick and
+	improving workflows has lead me to work across cultures and industries, including: the UK, Japan & Canada, designing
+	for non-profits, bespoke projects, hospitality, government bodies and property developers. Have a look at my work.
 </p>
+
+<section id="cv">
+	<ul>
+		<li>
+			<h2>The Guardian</h2>
+			<h3>2010-now</h3>
+			<p>
+				Assuming a senior software engineering role I work on the web platform,
+				<a href="https://www.theguardian.com/">TheGuardian.com</a>. Initially within the commercial development
+				team, then as part of the Dotcom team, who oversees the web platform in its entirety.
+			</p>
+			<p>
+				My main focus has been: modernisation JavaScript code, moving from Flow to TypeScript; optimisation of
+				vector images <abbr title="Scalable Vector Graphics">(SVG)</abbr>; various visualisations;
+			</p>
+		</li>
+
+		<li>
+			<h2>MXDVL</h2>
+			<h3>2010-2020</h3>
+			<p>
+				With over 10 years of experience building for the web, I have offered services ranging from fully
+				managed websites to transitioning away from legacy platforms. In the last 5 years, I have focussed on
+				bringing a single source of truth and 2D and 3D visualisation.
+			</p>
+			<p>
+				Designed and built entire websites with news, portfolio and forms. Improved Google page speed insight by
+				40-60 points on average on consultancy projects, meaning all web pages load fast. This was achieved by
+				specifying better cache rules, optimizing database requests and leveraging distributed CDNs. World
+				location mapping and styling with Leaflet. Integrated online payment with Stripe. Developed JSON APIs in
+				PHP to expose data to collaborators. Conducted surveys in multiple languages and combined the data into
+				live online spreadsheets.
+			</p>
+		</li>
+
+		<li>
+			<h2>Allies and Morrison LLP</h2>
+			<h3>2018-2020</h3>
+			<p>
+				Assuming a role of building information modelling lead (BIM), I developed internal tooling workflow,
+				oversaw data exchange with consultants and advised on technical improvements to the <a
+					href="https://www.alliesandmorrison.com">company website</a
+				>.
+			</p>
+			<p>
+				Built testing tools for key project metrics using Grasshopper, ranging from sustainability to unit mix.
+				Developed solar analysis tools leading to major savings in building envelope performance. Leveraged BIM
+				to deliver project documentation on tight deadlines and trained the team throughout, exceeding targets.
+				Streamlined internal scheduling systems using automation & PowerQuery.
+			</p>
+		</li>
+
+		<li>
+			<h2>Florian Busch Architects</h2>
+			<h3>2016-2018</h3>
+			<p>
+				Assuming a role of technical lead, I helped defined and implement data-driven design within the
+				practice, an internal wiki and the <a href="https://www.florianbusch.com">public website</a>.
+			</p>
+			<p>
+				Developed GIS analysis tools for development of a ski resort in Hokkaido. Produced pictures of an
+				alternative 2020 Tokyo Olympic stadium, which was published in The Japan Times and ReThink Tokyo. Built
+				complex digital generative and data-driven models. Taught scripting training sessions to fellow
+				colleagues and improved workflow.
+			</p>
+			<p>
+				Oversaw the transition of the office server to a faster, more robust and easy to extend infrastructure.
+				The new system further allowed partners to access the office server remotely via VPN, a key feature when
+				presenting projects to oversea clients. Built custom tools that were critical in winning two major
+				projects in Hokkaido. Both relied on making complex spatial analysis expressed as simple and rich
+				diagrams.
+			</p>
+		</li>
+
+		<li>
+			<h2>Provencher_Roy</h2>
+			<h3>2015-2016</h3>
+			<p>
+				Operated and maintained the laser cutter. Visualisation of points cloud / 3D scans for grade-listed
+				building refurbishments
+			</p>
+		</li>
+	</ul>
+</section>
+
+<style>
+	ul {
+		list-style-type: none;
+		padding: var(--grid-double) 0 0;
+		margin: 0;
+	}
+
+	ul > li {
+		width: calc(36 * var(--grid));
+		max-width: 100%;
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		position: relative;
+		padding: 0 0 calc(3 * var(--grid));
+	}
+
+	ul > li:nth-last-child(1) {
+		padding-bottom: 0;
+	}
+	ul > li:nth-last-child(n + 2)::after {
+		content: " ";
+		width: 100%;
+		position: absolute;
+		border-bottom: 2px solid var(--skies);
+		bottom: calc(var(--grid-double) - 1px);
+	}
+
+	ul > li p {
+		width: 100%;
+	}
+</style>
