e90b2f5fce209f36ed5ae4b50faeb4e73bca8b8a
Max <hi@mxdvl.com>
2022-01-15 19:00:34 +0000

    style buttons uniquely

---
+++ dotcom/src/lib/Button.svelte
@@ -0,0 +1,49 @@
+<button on:click>
+	<slot />
+</button>
+
+<style>
+	button {
+		--border: var(--ocean);
+
+		position: relative;
+
+		display: inline;
+		border: none;
+		padding: 0 3px;
+		margin: 0;
+		height: var(--grid);
+		color: inherit;
+		font-family: inherit;
+		font-size: inherit;
+		font-style: inherit;
+		font-weight: inherit;
+		line-height: inherit;
+		background: transparent;
+	}
+
+	button::before {
+		content: "";
+		position: absolute;
+		top: -1px;
+		left: -1px;
+		right: -1px;
+		bottom: -1px;
+
+		border-radius: 2px;
+		border: 2px solid var(--border);
+	}
+
+	button:disabled::before {
+		border-color: var(--skies);
+	}
+
+	button:enabled:hover {
+		cursor: pointer;
+		color: var(--border);
+	}
+
+	button:active {
+		--border: var(--glint);
+	}
+</style>
