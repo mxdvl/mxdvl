1d1df5bdc71b4556c236f0dadc19e1f4bc733f3f
Max <hi@mxdvl.com>
2022-01-18 21:42:00 +0000

    add caching mechanism

--- dotcom/api/greetings/[name].ts
+++ dotcom/api/greetings/[name].ts
@@ -1,18 +1,37 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-let count = 0;
+const cache: Record<
+	string,
+	{
+		timestamp: number;
+		data: unknown;
+	}
+> = {};
+
+const SERVER_EXPIRE = 15 * 60;
+const CLIENT_EXPIRE = 3 * 60;
 
 export default (request: VercelRequest, response: VercelResponse) => {
+	const now = new Date().getTime();
 	const { name } = request.query;
 
-	const SERVER_EXPIRE = 15 * 60;
-	const CLIENT_EXPIRE = 3 * 60;
+	const key = `${name}`;
+	if (cache[key].timestamp + SERVER_EXPIRE > now) delete cache[key];
+	if (!cache[key])
+		cache[key] = {
+			timestamp: now,
+			data: Math.random() * 1200,
+		};
+
+	const { data } = cache[key];
+
 	response.setHeader("Cache-Control", `public, maxage=${CLIENT_EXPIRE}, s-maxage=${SERVER_EXPIRE}`);
 
 	const body = {
 		greeting: `Hello ${name}! Comment allez-vous?`,
+		data,
 		date: new Date().toISOString(),
-		count: count++,
+		key,
 	};
 
 	response.status(200).json(body);
