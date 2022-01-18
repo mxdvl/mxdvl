d24beef6e3edde92de3b5cf61c28d59d90e9123e
Max <hi@mxdvl.com>
2022-01-18 20:32:09 +0000

    mark it public

--- dotcom/api/greetings/[name].ts
+++ dotcom/api/greetings/[name].ts
@@ -7,7 +7,7 @@ export default (request: VercelRequest, response: VercelResponse) => {
 
 	const SERVER_EXPIRE = 15 * 60;
 	const CLIENT_EXPIRE = 3 * 60;
-	response.setHeader("Cache-Control", `maxage=${CLIENT_EXPIRE}, s-maxage=${SERVER_EXPIRE}`);
+	response.setHeader("Cache-Control", `public, maxage=${CLIENT_EXPIRE}, s-maxage=${SERVER_EXPIRE}`);
 
 	const body = {
 		greeting: `Hello ${name}! Comment allez-vous?`,
