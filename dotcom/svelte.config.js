5773628dc2670f1ee5cadd3fd465558cc23c40e6
Max <hi@mxdvl.com>
2022-01-23 16:52:02 +0000

    try auto adapter

--- dotcom/svelte.config.js
+++ dotcom/svelte.config.js
@@ -13,10 +13,6 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter(),
 		target: "#svelte",
-		headers: {
-			host: "X-Forwarded-Host",
-			protocol: "X-Forwarded-Proto",
-		},
 		prerender: {
 			entries: ["/hi", "/allo", "/error", "/erreur"],
 		},
