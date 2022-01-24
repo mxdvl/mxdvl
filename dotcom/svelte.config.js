fe74623f11b7851ed708b311a7b631e20792c6ed
Max <hi@mxdvl.com>
2022-01-24 10:37:48 +0000

    small config changes

--- dotcom/svelte.config.js
+++ dotcom/svelte.config.js
@@ -1,13 +1,9 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
-import { mdsvex } from "mdsvex";
-
-const extensions = [".svelte", ".md"];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions,
-	preprocess: [preprocess(), mdsvex({ extensions })],
+	preprocess: [preprocess()],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
