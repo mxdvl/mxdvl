d0ce0c58c46e3ab476c27d859dda62bcefbdb2e5
Max <hi@mxdvl.com>
2022-02-21 21:20:27 +0000

    add apple-touch-icons

--- dotcom/static/generate-favicon.sh
+++ dotcom/static/generate-favicon.sh
@@ -8,3 +8,10 @@ convert -background transparent \
 	favicon.png \
 	-define icon:auto-resize=64,32,16 \
 	favicon.ico
+
+
+# VIPS for apple-touch-icons
+
+vips copy cmps-icon-light.svg cmps-icon-light.png
+vips copy cmps-icon-dark.svg cmps-icon-dark.png
+cp cmps-icon-dark.png apple-touch-icon.png
