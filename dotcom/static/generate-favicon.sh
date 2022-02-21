59fe5c6dc5c5fd9d1a81b44d29a3c3136b2aa3cc
Max <hi@mxdvl.com>
2022-02-21 21:22:54 +0000

    copy to content

--- dotcom/static/generate-favicon.sh
+++ dotcom/static/generate-favicon.sh
@@ -15,3 +15,10 @@ convert -background transparent \
 vips copy cmps-icon-light.svg cmps-icon-light.png
 vips copy cmps-icon-dark.svg cmps-icon-dark.png
 cp cmps-icon-dark.png apple-touch-icon.png
+
+
+# Copy to content
+cp apple-touch-icon.png ../../content/static
+cp favicon.ico ../../content/static
+cp favicon.png ../../content/static
+cp mask-icon.svg ../../content/static
