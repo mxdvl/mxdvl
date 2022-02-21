c8dfbb48730ab7484a39e7a4c324a2bac2064046
Max Duval <hi@mxdvl.com>
2022-02-21 23:06:22 +0000

    Apple touch icon (#62)

--- dotcom/static/generate-favicon.sh
+++ dotcom/static/generate-favicon.sh
@@ -8,3 +8,17 @@ convert -background transparent \
 	favicon.png \
 	-define icon:auto-resize=64,32,16 \
 	favicon.ico
+
+
+# VIPS for apple-touch-icons
+
+vips copy cmps-icon-light.svg cmps-icon-light.png
+vips copy cmps-icon-dark.svg cmps-icon-dark.png
+cp cmps-icon-dark.png apple-touch-icon.png
+
+
+# Copy to content
+cp apple-touch-icon.png ../../content/static
+cp favicon.ico ../../content/static
+cp favicon.png ../../content/static
+cp mask-icon.svg ../../content/static
