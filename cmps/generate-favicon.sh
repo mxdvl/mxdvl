3e66681edd3a13562a98114e2dc992968e6a7621
Max Duval <hi@mxdvl.com>
2022-09-09 17:14:14 +0100

    fixup

--- cmps/generate-favicon.sh
+++ cmps/generate-favicon.sh
@@ -1,26 +1,27 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 set -e
 
 # VIPS for SVG to PNG
 
-vips copy cmps.svg favicon.png
+vips copy cmps.svg build/favicon.png
 
 # Convert PNG to ICO
 convert -background transparent \
-	favicon.png \
+	build/favicon.png \
 	-define icon:auto-resize=64,32,16 \
-	favicon.ico
+	build/favicon.ico
 
 
 # VIPS for apple-touch-icons
 
-vips copy cmps-icon-light.svg cmps-icon-light.png
-vips copy cmps-icon-dark.svg cmps-icon-dark.png
-cp cmps-icon-dark.png apple-touch-icon.png
+vips copy cmps-icon-light.svg build/cmps-icon-light.png
+vips copy cmps-icon-dark.svg build/cmps-icon-dark.png
+cp build/cmps-icon-dark.png build/apple-touch-icon.png
 
+cp *.(svg|png|ico) build
 
 # Copy to dotcom, content & deno
-cp *.(svg|png|ico) ../content/static
-cp *.(svg|png|ico) ../dotcom/static
-cp *.(svg|png|ico) ../deno/static
+cp -R build/ ../dotcom/static
+cp -R build/ ../content/static
+cp -R build/ ../deno/static
