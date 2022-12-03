70a7b43917fd40e80d08ca1983496aaa61460ddc
Max Duval <hi@mxdvl.com>
2022-12-03 13:37:05 +0000

    Temp Deno branch (#117)
    feat(deno): temp server

--- deno/server.ts
+++ deno/server.ts
@@ -1,3 +1,16 @@
-console.info(
-	"nothing to see here until https://github.com/mxdvl/mxdvl/pull/110"
+import { serve } from "https://deno.land/std@0.167.0/http/server.ts";
+const port = 8080;
+
+await serve(
+	() =>
+		new Response(
+			'nothing to see here until <a href="https://github.com/mxdvl/mxdvl/pull/110">mxdvl#110</a>',
+			{
+				status: 200,
+				headers: {
+					"Content-Type": "text/html",
+				},
+			}
+		),
+	{ port }
 );
