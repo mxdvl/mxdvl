c6344d87020307df2a7a6cbe5aef9d59dead05aa
Max Duval <hi@mxdvl.com>
2022-12-14 09:26:03 +0000

    fix(lang): server-side store

--- dotcom/src/routes/+layout.ts
+++ dotcom/src/routes/+layout.ts
@@ -1,8 +1,9 @@
-import { pathLang } from "$lib/lang";
+import { lang as langStore, pathLang } from "$lib/lang";
 import type { LayoutLoad } from "./$types";
 
 export const load: LayoutLoad = async ({ url }) => {
 	const lang = pathLang(url.pathname);
+	langStore.set(lang);
 	return {
 		lang,
 	};
