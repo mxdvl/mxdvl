c2d1adc2feaf9ec2c3350eecf37513a25f5aa31f
Max Duval <hi@mxdvl.com>
2022-12-30 17:39:56 +0000

    fix(json): correct works endpoint

--- dotcom/src/routes/works/+page.server.ts
+++ dotcom/src/routes/works/+page.server.ts
@@ -3,7 +3,7 @@ import type { WorkUrls } from "@mxdvl/content/routes/works/+server";
 import { base } from "$lib/base";
 
 export const load: PageServerLoad = async () => {
-	const url = new URL("/works", base).toString();
+	const url = new URL("/works.json", base).toString();
 	const works: WorkUrls[] = await fetch(url).then((r) => r.json());
 
 	return { works };
