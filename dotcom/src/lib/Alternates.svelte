5a78757419c4fb05d696a7f1271a79ff33982bab
Max Duval <hi@mxdvl.com>
2023-01-11 13:42:27 +0000

    fix(Alternates): fully-qualified hrefs
    https://developer.mozilla.org/en-US/docs/Web/HTML/Element/link

--- dotcom/src/lib/Alternates.svelte
+++ dotcom/src/lib/Alternates.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <svelte:head>
-	<link rel="alternate" hreflang="en" href={en} />
-	<link rel="alternate" hreflang="fr" href={fr} />
+	<link rel="alternate" hreflang="en" href={new URL(en, 'https://www.mxdvl.com/').href} />
+	<link rel="alternate" hreflang="fr" href={new URL(fr, 'https://www.mxdvl.com/').href} />
 </svelte:head>
