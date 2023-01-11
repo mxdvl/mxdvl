b6f42410535bd668746126c02c3f20ccfc5f8e42
Max Duval <hi@mxdvl.com>
2023-01-11 17:54:47 +0000

    fix(grid): add all column variations

---
+++ dotcom/src/lib/grid.ts
@@ -0,0 +1,14 @@
+const GRID_SIZE = 18;
+
+export const grids = Array.from({ length: 10 }, (_, i) => (i + 3) * 6)
+	.map(
+		(cols) => `
+@media screen and (min-width: ${(cols + 1) * GRID_SIZE}px) {
+	#grid {
+		--columns: ${cols};
+	}
+}`,
+	)
+	.join("\n");
+
+console.log(grids);
