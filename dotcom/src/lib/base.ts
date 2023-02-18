dc949acf13f2d9f13b2042942e5c4ea1831b2486
Max Duval <hi@mxdvl.com>
2023-02-18 21:54:08 +0000

    feat: remove content

--- dotcom/src/lib/base.ts
+++
@@ -1,3 +0,0 @@
-const isDev = process.env.NODE_ENV === "development";
-
-export const base = isDev ? "http://localhost:3002/" : "https://content.mxdvl.com/";
--- dotcom/src/routes/(💼)/works/[slug]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import WorkComponent from "$lib/Work.svelte";
-	import type { PageData } from "./$types";
-
-	export let data: PageData;
-	const { work } = data;
-</script>
-
-<WorkComponent {work} />
