eabf2002836d3734b05fbf3acd92fd244e59d288
Max Duval <hi@mxdvl.com>
2023-02-27 00:09:59 +0000

    feat(favicon): generate with Image Magick
    no reliance on VIPS or --allow-run
    
    ```
    identify cmps/build/favicon.ico
    cmps/build/favicon.ico[0] ICO 128x128 128x128+0+0 8-bit sRGB 0.010u 0:00.001
    cmps/build/favicon.ico[1] ICO 64x64 64x64+0+0 8-bit sRGB 0.010u 0:00.001
    cmps/build/favicon.ico[2] ICO 32x32 32x32+0+0 8-bit sRGB 0.010u 0:00.000
    cmps/build/favicon.ico[3] ICO 16x16 16x16+0+0 8-bit sRGB 90022B 0.010u 0:00.000
    ```

--- deno/generate_favicons.ts
+++ deno/generate_favicons.ts
@@ -1,6 +1,7 @@
 import { copy } from "std/fs/copy.ts";
 import { cyan, green, red } from "std/fmt/colors.ts";
 import { render } from "resvg";
+import { generate_favicon } from "./scripts/ico.ts";
 
 console.log("Generating all favicons");
 
@@ -8,51 +9,19 @@ const cmps = new URL("../cmps", import.meta.url);
 
 const cwd = await Deno.realPath(cmps);
 
-const deps = ["vips", "convert"] as const;
-
-for (const dep of deps) {
-	const process = Deno.run({ cwd, cmd: ["which", dep] });
-	const { code } = await process.status();
-	if (code !== 0) Deno.exit(code);
-}
-
-const commands: Array<{
-	name: string;
-	cmd: [(typeof deps)[number], ...string[]];
-}> = [
-	{
-		name: "Vips – for SVG to PNG",
-		cmd: ["vips", "copy", "cmps.svg", "build/favicon.png"],
-	},
-	{
-		name: "ImageMagick – Convert PNG to ICO",
-		cmd: [
-			"convert",
-			"-background",
-			"transparent",
-			"build/favicon.png",
-			"-define",
-			`icon:auto-resize=${[64, 32, 16]}`,
-			"build/favicon.ico",
-		],
-	},
-];
-
 await Deno.mkdir(`${cwd}/build`, { recursive: true });
-
-for (const { name, cmd } of commands) {
-	console.info(name);
-	const process = Deno.run({ cwd, cmd });
-	const { code } = await process.status();
-	if (code !== 0) Deno.exit(code);
-}
 
 for await (const { name, isFile } of Deno.readDir(cwd)) {
 	if (isFile && name.endsWith(`.svg`)) {
 		Promise.allSettled([
 			Deno.readTextFile(`${cwd}/${name}`)
 				.then((svg) => render(svg))
-				.then((png) => Deno.writeFile(`${cwd}/build/${name.replace(".svg", ".png")}`, png)),
+				.then((png) =>
+					Promise.all([
+						Deno.writeFile(`${cwd}/build/${name.replace(".svg", ".png")}`, png),
+						name === "cmps.svg" ? generate_favicon(`${cwd}/build/favicon.ico`, png) : undefined,
+					])
+				),
 			Deno.copyFile(`${cwd}/${name}`, `${cwd}/build/${name}`),
 		]).then((steps) => {
 			const status = steps.every(({ status }) => status === "fulfilled") ? green("○") : red("×");
