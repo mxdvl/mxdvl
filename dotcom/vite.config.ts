8290a219f822141ca30b90ed3767e50157d23819
Max Duval <hi@mxdvl.com>
2023-03-19 22:30:14 +0000

    feat(https): safely serve locally

--- dotcom/vite.config.ts
+++ dotcom/vite.config.ts
@@ -1,10 +1,28 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { imagetools } from "vite-imagetools";
 import { defaultDirectives } from "./src/lib/picture";
-import type { UserConfig } from "vite";
+import type { ConfigEnv, UserConfigFn } from "vite";
+import { readFile } from "node:fs/promises";
 
-const config = {
+const get_certificate = async (command: ConfigEnv["command"]) => {
+	try {
+		return command === "serve"
+			? {
+					https: {
+						key: await readFile(".cert/key.pem"),
+						cert: await readFile(".cert/cert.pem"),
+					},
+			  }
+			: undefined;
+	} catch (error) {
+		console.warn("Failed to load SSL certificates. Did you run 'pnpm cert'?");
+		return undefined;
+	}
+};
+
+const config: UserConfigFn = async ({ command }) => ({
 	plugins: [sveltekit(), imagetools({ defaultDirectives })],
-} satisfies UserConfig;
+	server: await get_certificate(command),
+});
 
 export default config;
