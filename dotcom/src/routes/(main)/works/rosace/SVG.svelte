b44ed42f8af4026d07df5caebca378a0bf6fd9e2
Max Duval <hi@mxdvl.com>
2023-04-10 21:20:20 +0100

    fix: more discernable shapes

--- dotcom/src/routes/(main)/works/rosace/SVG.svelte
+++ dotcom/src/routes/(main)/works/rosace/SVG.svelte
@@ -5,6 +5,20 @@
 	const origin = centre ? `${-size / 2},${-size / 2}` : "0,0";
 </script>
 
-<svg viewBox={`${origin} ${size},${size}`} width={size} height={size} stroke="black" stroke-width={1} fill="none">
+<svg
+	viewBox={`${origin} ${size},${size}`}
+	width={size}
+	height={size}
+	stroke="var(--earth)"
+	stroke-width={1}
+	fill="none"
+>
+	<rect x={-size} y={-size} width={size * 2} height={size * 2} fill="var(--clouds)" />
 	<slot />
 </svg>
+
+<style>
+	svg {
+		border: 2px solid var(--skies);
+	}
+</style>
