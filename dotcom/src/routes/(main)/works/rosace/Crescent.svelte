b54e9d6a7ebc673b2113a9cc98eae3bbc3726466
Max Duval <hi@mxdvl.com>
2023-04-11 08:10:46 +0100

    refactor: crescent names

--- dotcom/src/routes/(main)/works/rosace/Crescent.svelte
+++ dotcom/src/routes/(main)/works/rosace/Crescent.svelte
@@ -5,7 +5,22 @@
 
 	let radius = 6;
 
+	const get_name = (radius: number) => {
+		switch (radius) {
+			case -18:
+				return "circle";
+			case 0:
+				return "semicircle";
+			case 18:
+				return "arc";
+
+			default:
+				return "crescent";
+		}
+	};
+
 	$: d = ["M0,18", "A18,18 0 0 1 0,-18", `A${Math.abs(radius)},18 0 0 ${radius > 0 ? "0" : "1"} 0,18`, "Z"].join("");
+	$: name = get_name(radius);
 </script>
 
 <SVG centre>
@@ -24,7 +39,7 @@
 		} else {
 			add_pattern(d);
 		}
-	}}>{$current ? "set to" : "add"} crescent</Button
+	}}>{$current ? "set to" : "add"} {name}</Button
 >
 
 <style>
