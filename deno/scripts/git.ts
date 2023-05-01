792660f51d61d5d45d50d6ee77a6a011aa4a9d89
Max Duval <hi@mxdvl.com>
2023-05-01 12:42:16 +0100

    refactor: use Deno.Command
    improve reliability & formatting

--- deno/scripts/git.ts
+++ deno/scripts/git.ts
@@ -1,38 +1,56 @@
-const list_branches = async () => {
-	const process = Deno.run({
-		cmd: ['git', 'branch', '-vv'],
-		stdout: 'piped',
+import { assert } from "std/testing/asserts.ts";
+
+const nonempty_string = (d: string | undefined): d is string => typeof d === "string" && d.length > 0;
+
+const fetch_all_branches = () => {
+	const cmd = new Deno.Command("git", {
+		args: ["fetch", "-p"],
 	});
 
+	return cmd.output();
+};
+
+const list_branches = async () => {
+	const cmd = new Deno.Command("git", {
+		args: ["branch", "-vv"],
+	});
+
+	const { success, stdout } = await cmd.output();
+
+	assert(success);
+
 	return new TextDecoder()
-		.decode(await process.output())
-		.split('\n')
+		.decode(stdout)
+		.split("\n")
 		.map((line) => line.trim())
-		.filter(Boolean);
+		.filter(nonempty_string);
 };
 
 const remove_gone_branches = async () => {
+	await fetch_all_branches();
 	const branches = await list_branches();
-	const gone_branches = branches.filter((branch) => branch.match(': gone]'))
-		.map((branch) => branch.split(' ')[0]);
+	const gone_branches = branches
+		.filter((branch) => branch.match(": gone]"))
+		.map((branch) => branch.split(" ")[0] ?? "")
+		.filter(nonempty_string);
 
 	if (!gone_branches.length) {
-		console.info('All branches exist on remote');
+		console.info("All branches exist on remote");
 		Deno.exit();
 	} else {
-		console.warn('Will remove the following branches', gone_branches);
+		console.warn("Will remove the following branches", gone_branches);
 	}
 
-	const proceed = confirm('Proceed?');
+	const proceed = confirm("Proceed?");
 	if (!proceed) {
-		console.log('Aborting, no branches removed');
+		console.log("Aborting, no branches removed");
 		Deno.exit();
 	}
 
-	const process = Deno.run({
-		cmd: ['git', 'branch', '-D', ...gone_branches],
+	const cmd = new Deno.Command("git", {
+		args: ["branch", "-D", ...gone_branches],
 	});
-	const { code } = await process.status();
+	const { code } = await cmd.output();
 	Deno.exit(code);
 };
 
