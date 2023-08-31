40aacd73da7c65ee1494df11973cce6e295ad06b
Max Duval <hi@mxdvl.com>
2023-08-31 14:56:03 +0100

    refactor: reintroduce fathom

--- deno/components/Layout.svelte
+++ deno/components/Layout.svelte
@@ -1,21 +1,21 @@
 <script>
 	import Header from "./Header.svelte";
 	import Footer from "./Footer.svelte";
-	// import { onMount } from "svelte";
-	// import { load, trackPageview } from "npm:fathom-client@3.5.0";
+	import { onMount } from "svelte";
+	import { load, trackPageview } from "npm:fathom-client@3.5.0";
 	import { capitalise } from "../../dotcom/src/lib/capitalise.js";
 
 	const url = new URL("/hi", "https://www.mxdvl.com/");
 
 	const lang = "en";
 
-	// onMount(() => {
-	// 	load("MDDFSRVF", {
-	// 		includedDomains: ["www.mxdvl.com"],
-	// 		url: "https://cdn.usefathom.com/script.js",
-	// 	});
-	// 	trackPageview();
-	// });
+	onMount(() => {
+		load("MDDFSRVF", {
+			includedDomains: ["www.mxdvl.com"],
+			url: "https://cdn.usefathom.com/script.js",
+		});
+		trackPageview();
+	});
 </script>
 
 <svelte:head>
