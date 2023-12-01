1f71b1d765b4ec9788a10f9ab559c08d4fb360f1
Max Duval <hi@mxdvl.com>
2023-12-01 20:49:10 +0000

    fix: formatting

--- dotcom/src/routes/(main)/aoc/2023/+page.server.ts
+++ dotcom/src/routes/(main)/aoc/2023/+page.server.ts
@@ -1,8 +1,7 @@
 import { paths } from "$lib/paths";
 
 export const load = async () => {
-
 	return {
-		days: paths(import.meta.url + "/../day/0").map(path => ["day", path].join("/")),
+		days: paths(import.meta.url + "/../day/0").map((path) => ["day", path].join("/")),
 	};
 };
