679e81ea97e4b2acb4c4d9373937578290ab37ab
Max Duval <hi@mxdvl.com>
2023-12-01 21:03:22 +0000

    fix: make paths work

---
+++ dotcom/src/routes/(main)/aoc/2023/+page.server.js
@@ -0,0 +1,11 @@
+import { paths } from "$lib/paths";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+export const load = async () => {
+	const dir = join(dirname(fileURLToPath(import.meta.url)), "day");
+
+	return {
+		days: paths(dir).map((path) => ["day", path].join("/")),
+	};
+};
--- dotcom/src/routes/(main)/aoc/2023/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { paths } from "$lib/paths";
-
-export const load = async () => {
-	return {
-		days: paths(import.meta.url + "/../day/0").map((path) => ["day", path].join("/")),
-	};
-};
