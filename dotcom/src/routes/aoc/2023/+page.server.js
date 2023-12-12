a4e311f9e89a6783dfc17874d3087324c6824259
Max Duval <hi@mxdvl.com>
2023-12-12 09:31:19 +0000

    chore: prettier

--- dotcom/src/routes/aoc/2023/+page.server.js
+++ dotcom/src/routes/aoc/2023/+page.server.js
@@ -6,6 +6,8 @@ export const load = async () => {
 	const dir = join(dirname(fileURLToPath(import.meta.url)), "day");
 
 	return {
-		days: paths(dir).sort((a,b) => Number(a) - Number(b)).map((path) => ["day", path].join("/")),
+		days: paths(dir)
+			.sort((a, b) => Number(a) - Number(b))
+			.map((path) => ["day", path].join("/")),
 	};
 };
