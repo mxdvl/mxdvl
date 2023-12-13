00d670385f4d2fba21039f57f7c7d5a2deee6883
Max Duval <hi@mxdvl.com>
2023-12-13 23:38:06 +0000

    feat(day 8): improve d3 types

--- dotcom/src/routes/aoc/2023/day/8/NetworkGraph.svelte
+++ dotcom/src/routes/aoc/2023/day/8/NetworkGraph.svelte
@@ -1,16 +1,13 @@
-<script>
+<script lang="ts">
 	import { onMount } from "svelte";
 
-	import { zoom } from "d3-zoom";
-	import { select } from "d3-selection";
-	import { drag } from "d3-drag";
-	import { forceSimulation, forceLink, forceManyBody, forceCenter } from "d3-force";
+	import { zoom, type D3ZoomEvent } from "d3-zoom";
+	import { select, type Selection } from "d3-selection";
+	import { drag, type D3DragEvent } from "d3-drag";
+	import { forceSimulation, forceLink, forceManyBody, forceCenter, type Simulation } from "d3-force";
+	import type { LinkDatum, NodeDatum } from "./types";
 
-	/** @typedef {import('./types.js').NodeDatum} NodeDatum */
-	/** @typedef {import('./types.js').LinkDatum} LinkDatum */
-
-	/** @type {{nodes: NodeDatum[], links: LinkDatum[]}} */
-	export let graph = { links: [], nodes: [] };
+	export let graph: { nodes: NodeDatum[]; links: LinkDatum[] } = { links: [], nodes: [] };
 
 	export let width = 600;
 	export let height = 600;
@@ -21,20 +18,22 @@
 
 	const colours = { Z: "var(--red)", A: "var(--green)", node: "currentColor" };
 
-	/** @param {NodeDatum} d */
-	const center = (d) => ({
+	const center = (d: NodeDatum) => ({
 		x: (d.group % 3) * ((2.5 * width) / starting_points.length) - width / 2,
 		y: Math.floor(d.group / 3) * ((4 * height) / starting_points.length) - height / 2,
 	});
 
-	/** @type {import('d3-force').Simulation<NodeDatum, LinkDatum>} */
-	let simulation;
-	/** @type {import('d3-selection').Selection<SVGElement, unknown, HTMLElement, unknown>} */
-	let svg;
+	let simulation: Simulation<NodeDatum, LinkDatum>;
+
+	let svg: Selection<SVGElement, unknown, HTMLElement, unknown>;
 
 	$: {
 		for (const link of links) {
-			if (!nodes.some((d) => d.id === link.target)) nodes.push({ id: link.target, group: -1 });
+			if (!nodes.some((d) => d.id === link.target))
+				nodes.push(
+					// @ts-expect-error -- it’s D3
+					{ id: link.target, group: -1 },
+				);
 		}
 		nodes = nodes;
 	}
@@ -42,7 +41,7 @@
 		simulation = forceSimulation(nodes)
 			.force(
 				"link",
-				forceLink(links)
+				forceLink<NodeDatum, LinkDatum>(links)
 					.id((d) => d.id)
 					.strength(0.8),
 			)
@@ -80,7 +79,10 @@
 					    ? colours.node
 					    : "var(--blue)",
 			)
-			.call(drag().on("start", dragstarted).on("drag", dragged).on("end", dragended));
+			.call(
+				// @ts-expect-error -- it’s D3
+				drag().on("start", dragstarted).on("drag", dragged).on("end", dragended),
+			);
 
 		node.append("title").text((d) => {
 			const index = starting_points.findIndex((p) => p.id === d.id);
@@ -88,6 +90,7 @@
 		});
 
 		svg.call(
+			// @ts-expect-error -- it’s D3
 			zoom()
 				.extent([
 					[-width * 2, -height * 2],
@@ -98,35 +101,64 @@
 		);
 
 		function simulationUpdate() {
-			link.attr("x1", (d) => d.source.x)
-				.attr("y1", (d) => d.source.y)
-				.attr("x2", (d) => d.target.x)
-				.attr("y2", (d) => d.target.y);
+			link.attr(
+				"x1",
+				(d) =>
+					// @ts-expect-error -- it’s D3
+					d.source.x,
+			)
+				.attr(
+					"y1",
+					(d) =>
+						// @ts-expect-error -- it’s D3
+						d.source.y,
+				)
+				.attr(
+					"x2",
+					(d) =>
+						// @ts-expect-error -- it’s D3
+						d.target.x,
+				)
+				.attr(
+					"y2",
+					(d) =>
+						// @ts-expect-error -- it’s D3
+						d.target.y,
+				);
 
-			node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+			node.attr(
+				"cx",
+				// @ts-expect-error -- it’s D3
+				(d) => d.x,
+			).attr(
+				"cy",
+				// @ts-expect-error -- it’s D3
+				(d) => d.y,
+			);
 		}
 
-		function zoomed(event) {
-			g.attr("transform", event.transform);
+		function zoomed(event: D3ZoomEvent<SVGGeometryElement, NodeDatum>) {
+			g.attr(
+				"transform",
+				// @ts-expect-error -- it’s D3
+				event.transform,
+			);
 			simulationUpdate();
 		}
 	});
 
-	/** @param {import('d3-drag').D3DragEvent<HTMLElement, unknown, NodeDatum>} event */
-	function dragstarted(event) {
+	function dragstarted(event: D3DragEvent<HTMLElement, unknown, NodeDatum>) {
 		if (!event.active) simulation.alphaTarget(0.3).restart();
 		event.subject.fx = event.x;
 		event.subject.fy = event.y;
 	}
 
-	/** @param {import('d3-drag').D3DragEvent<HTMLElement, unknown, NodeDatum>} event */
-	function dragged(event) {
+	function dragged(event: D3DragEvent<HTMLElement, unknown, NodeDatum>) {
 		event.subject.fx = event.x;
 		event.subject.fy = event.y;
 	}
 
-	/** @param {import('d3-drag').D3DragEvent<HTMLElement, unknown, NodeDatum>} event */
-	function dragended(event) {
+	function dragended(event: D3DragEvent<HTMLElement, unknown, NodeDatum>) {
 		if (!event.active) simulation.alphaTarget(0);
 		event.subject.fx = null;
 		event.subject.fy = null;
--- dotcom/src/routes/aoc/2023/day/8/types.js
+++ dotcom/src/routes/aoc/2023/day/8/types.js
@@ -1,4 +1,4 @@
 /** @typedef {{id: string, group: number} & import('d3-force').SimulationNodeDatum} NodeDatum */
-/** @typedef {{source: string, target: string} & import('d3-force').SimulationLinkDatum<NodeDatum>} LinkDatum */
+/** @typedef {import('d3-force').SimulationLinkDatum<NodeDatum>} LinkDatum */
 
 export {};
