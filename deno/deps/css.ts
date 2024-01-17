9b3d304ce1a68a47e17abbf68fba86498d4373c9
Max Duval <hi@mxdvl.com>
2024-01-17 11:19:21 +0000

    fix(import): use with { type: "json" }

--- deno/deps/css.ts
+++ deno/deps/css.ts
@@ -1,5 +1,5 @@
 import init, { transform } from "lightningcss-wasm";
-import import_map from "../import-map.json" assert { type: "json" };
+import import_map from "../import-map.json" with { type: "json" };
 
 await init(import_map.imports["lightningcss-wasm"] + "/lightningcss_node.wasm?module");
 
