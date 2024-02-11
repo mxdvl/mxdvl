084e4945b31fec6cebd2bf435816d8b92a2f0e94
Max Duval <hi@mxdvl.com>
2024-02-11 13:20:11 +0000

    fix: handle all cases

--- dotcom/src/routes/(main)/tools/svg/+page.svelte
+++ dotcom/src/routes/(main)/tools/svg/+page.svelte
@@ -5,10 +5,14 @@
 	/** @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d */
 	let d = SVGPathCommander.pathToString([
 		["m", 3, 3],
-		["l", 60, 20],
-		["l", 120, 0],
-		["l", 0, 60],
-		["c", 20, 0, 60, -30, 20, -20],
+		["l", 60, 40],
+		["a", 90, 120, 30, 0, 1, 120, 0],
+		["l", -20, 60],
+		["q", -20, 60, 20, 20],
+		["l", 60, 0],
+		["c", 60, 0, 60, -20, 20, -20],
+		["c", 40, 0, 40, -20, 0, -20],
+		["c", 40, 0, 40, -20, -20, -20],
 	]);
 
 	const size = 360;
@@ -16,7 +20,7 @@
 	/** @param {string} d */
 	const getPathCommander = (d) => {
 		try {
-			return new SVGPathCommander(d);
+			return new SVGPathCommander(d).normalize();
 		} catch (_) {
 			return undefined;
 		}
@@ -26,10 +30,7 @@
 </script>
 
 <svg viewBox={`0,0 ${size},${size}`} width={size} height={size} stroke="var(--earth)" stroke-width={2} fill="none">
-	<Path visualise={true} {d} />
-	<g transform="translate(20 20)">
-		<Path visualise={true} d={pathCommander?.normalize.toString() ?? ""} />
-	</g>
+	<Path visualise={true} d={pathCommander?.normalize().toRelative().toString() ?? ""} />
 </svg>
 
 <textarea cols="30" bind:value={d} />
--- dotcom/src/routes/(main)/tools/svg/Path.svelte
+++ dotcom/src/routes/(main)/tools/svg/Path.svelte
@@ -1,19 +1,82 @@
 <script>
+	import SVGPathCommander from "svg-path-commander";
+
 	export let visualise = false;
 	/** @type {string} */
 	export let d;
 
-	const marker = visualise ? "url(#dot)" : undefined;
+	/** @typedef {{ x: number, y: number, segment: import('svg-path-commander').NormalSegment}} Segment */
+
+	/** @param {string} d */
+	const getHandles = (d) => {
+		try {
+			return SVGPathCommander.normalizePath(d).reduce(
+				({ x0, y0, handles }, segment) => {
+					switch (segment[0]) {
+						case "M": {
+							return { x0: segment[1], y0: segment[2], handles };
+						}
+						case "L": {
+							const [, x1, y1] = segment;
+							handles.push(`M${x0},${y0}L${x1},${y1}`);
+							return { x0: segment[1], y0: segment[2], handles };
+						}
+						case "C": {
+							const [, x1, y1, x2, y2, x3, y3] = segment;
+							handles.push(`M${x0},${y0}L${x1},${y1}`);
+							handles.push(`M${x3},${y3}L${x2},${y2}`);
+							return { x0: segment[5], y0: segment[6], handles };
+						}
+						case "Q": {
+							const [, x1, y1, x2, y2] = segment;
+							handles.push(`M${x0},${y0}L${x1},${y1} ${x2},${y2}`);
+							return { x0: segment[3], y0: segment[4], handles };
+						}
+						case "A": {
+							const [, rx, ry, angle, large, sweep, x1, y1] = segment;
+							handles.push(`M${x0},${y0}L${x1},${y1}`);
+							handles.push(`M${x0},${y0}A${rx},${ry},${angle},${1 - large},${1 - sweep},${x1},${y1}`);
+							return { x0: segment[6], y0: segment[7], handles };
+						}
+						case "Z": {
+							return { x0: 0, y0: 0, handles };
+						}
+					}
+				},
+				{ x0: 0, y0: 0, handles: /** @type {string[]} */ ([]) },
+			).handles;
+		} catch (_) {
+			return [];
+		}
+	};
+
+	$: handles = visualise ? getHandles(d) : [];
 </script>
 
 <marker id="dot" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="5" markerHeight="5">
 	<circle cx="5" cy="5" r="3" fill="var(--glint)" stroke="none" />
 </marker>
 
-<path {d} marker-start={marker} marker-mid={marker} marker-end={marker} />
+{#each handles as handle}
+	<path
+		class="handles"
+		d={handle}
+		stroke-dasharray="1 6"
+		stroke-linecap="round"
+		marker-start="url(#dot)"
+		marker-mid="url(#dot)"
+		marker-end="url(#dot)"
+	/>
+{/each}
+
+<path {d} />
 
 <style>
 	path {
 		stroke: var(--earth);
+	}
+
+	path.handles {
+		stroke: var(--glint);
 	}
 </style>
