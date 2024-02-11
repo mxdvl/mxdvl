532bc4272cd7735548dcb01c0d0529e8ffd41f6b
Max Duval <hi@mxdvl.com>
2024-02-11 13:36:31 +0000

    SVG visualiser (#688)
    - visualise any path with handles
    - better export from Bobbin

---
+++ dotcom/src/routes/(main)/tools/svg/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Path from "./Path.svelte";
+	import SVGPathCommander from "svg-path-commander";
+
+	/** @see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d */
+	let d = SVGPathCommander.pathToString([
+		["m", 3, 3],
+		["l", 60, 40],
+		["a", 90, 120, 30, 0, 1, 120, 0],
+		["l", -20, 60],
+		["q", -20, 60, 20, 20],
+		["l", 60, 0],
+		["c", 60, 0, 60, -20, 20, -20],
+		["c", 40, 0, 40, -20, 0, -20],
+		["c", 40, 0, 40, -20, -20, -20],
+	]);
+
+	const size = 360;
+
+	/** @param {string} d */
+	const getSegments = (d) => {
+		try {
+			return SVGPathCommander.parsePathString(d);
+		} catch (_) {
+			return [];
+		}
+	};
+
+	$: segments = getSegments(d);
+</script>
+
+<svg viewBox={`0,0 ${size},${size}`} width={size} height={size} stroke="var(--earth)" stroke-width={2} fill="none">
+	<Path visualise={true} {d} />
+</svg>
+
+<textarea cols="30" bind:value={d} />
+
+<ul>
+	{#each segments as [command, ...parts]}
+		<li>
+			<strong>{command}</strong>: {parts.join(" ")}
+		</li>
+	{/each}
+</ul>
