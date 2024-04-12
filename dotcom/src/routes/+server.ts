708c319e33af76b188f27e676e37a117c1da71cb
Max Duval <hi@mxdvl.com>
2024-04-12 15:55:06 +0100

    fix: types

--- dotcom/src/routes/+server.ts
+++ dotcom/src/routes/+server.ts
@@ -1,11 +1,12 @@
+import type { Lang } from "$lib/lang";
+
 export const GET = ({ request }) => {
 	const maybeLang = request.headers
 		.get("accept-language")
 		?.split(",")
 		.find((str) => str.startsWith("fr") || str.startsWith("en"));
 
-	/** @type {Lang} */
-	const lang = maybeLang?.startsWith("fr") ? "fr" : "en";
+	const lang: Lang = maybeLang?.startsWith("fr") ? "fr" : "en";
 
 	return new Response(undefined, {
 		status: 301,
