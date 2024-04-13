8ee3c11fee7f776672c22118e0869ef854a6399f
Max Duval <hi@mxdvl.com>
2024-04-13 12:46:06 +0100

    More recent stuff (#778)

--- dotcom/src/routes/(main)/hi/+page.svelte
+++ dotcom/src/routes/(main)/hi/+page.svelte
@@ -29,10 +29,15 @@
 
 <ul>
 	<li>Deploying a real-time data collection pipeline focussed on privacy in April 2024</li>
+	<li>
+		Visualisating the <a href="https://observablehq.com/@mxdvl/equinoxe">journey of Equinoxe</a> towards the
+		<a href="https://transatqsm.com">Transat Québec–St-Malo</a>
+	</li>
+	<li><a href="/works/bobbin">Bobbin</a>, a tool for making intricate symmetries</li>
 	<li><a href="https://mxdvl.github.io/gudrid/">Gudrid</a>, a tool to explore Guardian content</li>
 	<li>
 		The launch of <a href="https://github.com/guardian/typescript-school">TypeScript school</a>, which helps all
-		developers be on the same page in November 2023.
+		developers be on the same page in November 2023
 	</li>
 	<li>
 		<a href="https://www.theguardian.com/info/2023/oct/24/standardisation-the-merit-of-consistent-patterns"
@@ -45,9 +50,9 @@
 		>
 	</li>
 	<li>Mentored four fellow engineers in 2023, which help me grow along with them</li>
-	<li><a href="/works/bobbin">Bobbin</a>, a tool for making intricate symmetries</li>
 	<li>
-		<a href="https://github.com/guardian/csnx/pull/850">20&times; speed improvement</a> on internal relative time method
+		Kept tinkering with ideas on <a href="https://codepen.io/mxdvl">CodePen</a> and
+		<a href="https://www.mxdvl.com/aoc/2023">Advent of Code 2023</a>
 	</li>
 </ul>
 
