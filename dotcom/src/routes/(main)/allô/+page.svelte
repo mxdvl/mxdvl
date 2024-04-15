5f6c21184418158505bab9976c56bec1839b26de
Max Duval <hi@mxdvl.com>
2024-04-15 11:26:57 +0100

    Davantage d’information (#779)
    * fix: make SSL work locally
    
    * feat(allô): liste plus complète

--- dotcom/src/routes/(main)/allô/+page.svelte
+++ dotcom/src/routes/(main)/allô/+page.svelte
@@ -9,20 +9,49 @@
 
 <Alternates en="/hi" fr="/allô" />
 
-<h1>Allô!</h1>
+<h1>Allô, c’est <strong>Max</strong>!</h1>
 
-<p>Allô, c’est Max.</p>
-
-<p>
-	Un dévelopeur & designer <a href="/hi">bilingue</a> à Londres
-	<span data-id={id}>{feels_like > 300 ? "bouillante" : getCondition(id, "fr")}</span>. Je travaille actuellement chez
-	The Guardian.
-</p>
+<h2>
+	Un dévelopeur & designer à Londres
+	<span data-id={id} data-temp={feels_like}>{feels_like > 300 ? "bouillante" : getCondition(id, "fr")}</span>.
+</h2>
 
 <p>
-	<a href="/travaux">Mon travail</a>
-	s’illustre par son élégance et sa simplicité. J’adore bâtir pour le web et
-	<a href="https://github.com/mxdvl">contribuer à du code source ouvert </a>.
+	Je construit actuellement le <a href="https://www.theguardian.com/uk">site du Guardian</a>. Il est possible d’en
+	voir plus sur mon parcours professionel sur <a href="/profil">mon profil</a>.
 </p>
 
-<p>Température ressentie {feels_like}K</p>
+<p>Récemment, je suis fier d’avoir accompli et participé aux projets suivants:</p>
+
+<ul>
+	<li>
+		Visualiser le <a href="https://observablehq.com/@mxdvl/equinoxe">trajet d’Equinoxe</a> vers la
+		<a href="https://transatqsm.com">Transat Québec–St-Malo</a>
+	</li>
+	<li><a href="/works/bobbin">Bobbin</a>, un outil pour tresser des symmétries délicates</li>
+	<li>
+		Le lancement de <a href="https://github.com/guardian/typescript-school">l’école TypeScript</a> (en anglais), qui
+		permet à tous les dévelopeurs d’être sur la même page
+	</li>
+	<li>Avoir été le mentor de quatre collègues ingénieur logiciels, avec lesquels je me suis développé</li>
+
+	<li>
+		Continuer à tripatouiller avec CSS & JS sur <a href="https://codepen.io/mxdvl">CodePen</a> et sur
+		<a href="https://www.mxdvl.com/aoc/2023">l’Advent of Code 2023</a>
+	</li>
+</ul>
+
+<p>
+	Comme je travaille principalement en Anglais, il y a <a href="/hi">plus d'info de ce côté-là</a>.
+</p>
+
+<style>
+	ul {
+		padding-left: var(--grid);
+		margin: 0;
+	}
+
+	li {
+		padding-bottom: var(--grid-y);
+	}
+</style>
