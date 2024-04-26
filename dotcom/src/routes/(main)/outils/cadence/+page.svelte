25401db0179a0db2bf04c0f63a767f0e47b45bea
Max Duval <hi@mxdvl.com>
2024-04-26 09:16:42 +0100

    refactor(cadence): only change on commit

--- dotcom/src/routes/(main)/outils/cadence/+page.svelte
+++ dotcom/src/routes/(main)/outils/cadence/+page.svelte
@@ -27,28 +27,17 @@
 	let perKilometre = initial;
 	let perMile = toMiles(initial);
 
-	/**
-	 * @param {number} newPace
-	 * @param {"km" | "m"} units
-	 */
-	const setPace = (newPace, units) => {
-		if (units === "km") perMile = toMiles(newPace);
-		if (units === "m") perKilometre = toKilometre(newPace);
-	};
-
-	$: setPace(perKilometre, "km");
-	$: setPace(perMile, "m");
 </script>
 
 <Alternates en="/tools/pace" fr="/outils/cadence" />
 
-<input type="range" bind:value={perKilometre} {min} {max} {step} />
+<input type="range" bind:value={perKilometre} {min} {max} {step} on:change={() => perMile = toMiles(perKilometre)} />
 
 <p>Une cadence de <strong>{toMinuteSeconds(perKilometre)} min / km</strong>…</p>
 
 <p>Équivaut <strong>{toMinuteSeconds(perMile)} min / mile</strong>!</p>
 
-<input type="range" bind:value={perMile} {min} {max} {step} />
+<input type="range" bind:value={perMile} {min} {max} {step} on:change={() => perKilometre = toKilometre(perMile)} />
 
 <style>
 	p {
