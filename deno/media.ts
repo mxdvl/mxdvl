03e15ec63ccb2f1a873ff12ef904834479fe5534
Max Duval <max.duval@hometrack.com>
2024-08-24 17:31:44 +0100

    chore: remove test website

--- deno/media.ts
+++ deno/media.ts
@@ -1,8 +1,5 @@
 import { ensureDir } from "jsr:@std/fs/ensure-dir";
 import { copy } from "jsr:@std/fs/copy";
-import { walk } from "jsr:@std/fs/walk";
-
-const match = [/\.(png|jpe?g)$/];
 
 const base = import.meta.url;
 
