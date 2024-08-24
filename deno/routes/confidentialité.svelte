898305d3190f103edd59cbbeb8ffb63cd645ca2e
Max Duval <max.duval@hometrack.com>
2024-08-24 15:49:42 +0100

    fix: tracking disabling

--- deno/routes/confidentialité.svelte
+++ deno/routes/confidentialité.svelte
@@ -1,36 +1,17 @@
 <script>
-	import { onMount } from "svelte";
-	import Button from "../components/Button.svelte";
 	import Layout from "../components/Layout.svelte";
 	import Alternates from "../components/Alternates.svelte";
+	import Tracking from "../components/Tracking.island.svelte";
 
-	let tracking = false;
-	onMount(() => {
-		tracking = localStorage.blockFathomTracking ? false : true;
-	});
-
-	const block = () => {
-		window.fathom?.blockTrackingForMe();
-		tracking = false;
-	};
-	const unblock = () => {
-		window.fathom?.enableTrackingForMe();
-		tracking = true;
-	};
+	const lang = "fr";
 </script>
 
 <Alternates en="/privacy" fr="/confidentialité" />
 
-<Layout lang="fr">
+<Layout {lang}>
 	<p>
 		Un suivi anonyme via <a href="https://usefathom.com/">Fathom</a> me permet de savoir quelles pages sont les plus
 		visitées. Vous pouvez toutefois choisir de désactiver ce suivi complètement ci-dessous.
 	</p>
-	<p>
-		{#if tracking}
-			Suivi anonyme <strong>actif</strong>. <Button on:click={block}>Désactiver complètement</Button>.
-		{:else}
-			Aucun suivi. <Button on:click={unblock}>Réactiver</Button>.
-		{/if}
-	</p>
+	<Tracking {lang} />
 </Layout>
--- deno/routes/privacy.svelte
+++ deno/routes/privacy.svelte
@@ -1,36 +1,18 @@
 <script>
 	import { onMount } from "svelte";
-	import Button from "../components/Button.svelte";
 	import Layout from "../components/Layout.svelte";
 	import Alternates from "../components/Alternates.svelte";
+	import Tracking from "../components/Tracking.island.svelte";
 
-	let tracking = false;
-	onMount(() => {
-		tracking = localStorage.blockFathomTracking ? false : true;
-	});
-
-	const block = () => {
-		window.fathom?.blockTrackingForMe();
-		tracking = false;
-	};
-	const unblock = () => {
-		window.fathom?.enableTrackingForMe();
-		tracking = true;
-	};
+	const lang = "en";
 </script>
 
 <Alternates en="/privacy" fr="/confidentialité" />
 
-<Layout lang="en">
+<Layout {lang}>
 	<p>
 		Anonymous tracking via <a href="https://usefathom.com/">Fathom</a> helps me understand which pages are the most seen.
 		You can still choose to opt out below.
 	</p>
-	<p>
-		{#if tracking}
-			Anonymous tracking <strong>active</strong>. <Button on:click={block}>Completely disable</Button>.
-		{:else}
-			No tracking whatsoever. <Button on:click={unblock}>Reactivate</Button>.
-		{/if}
-	</p>
+	<Tracking {lang} />
 </Layout>
