33350185346f41ec29a2ef86c7281876ae0af758
Max Duval <max.duval@hometrack.com>
2024-11-05 21:45:07 +0000

    refactor: write to build directly

--- deno/scripts/process_icons.ts
+++ deno/scripts/process_icons.ts
@@ -3,27 +3,27 @@ import { cyan, green, red } from "https://deno.land/std@0.193.0/fmt/colors.ts";
 import { walk } from "jsr:@std/fs";
 import { render } from "resvg";
 import { generate_favicon } from "./ico.ts";
+import { fromFileUrl } from "jsr:@std/path";
 
 console.log("Generating all favicons");
 
-const cmps = new URL("../../cmps", import.meta.url);
+const cmps = fromFileUrl(new URL("../../cmps", import.meta.url));
+const build = fromFileUrl(new URL("../../build", import.meta.url));
 
-const cwd = await Deno.realPath(cmps);
+await Deno.mkdir(build, { recursive: true });
 
-await Deno.mkdir(`${cwd}/build`, { recursive: true });
-
-for await (const { name } of walk(cwd, { includeDirs: false, match: [/\.svg$/], maxDepth: 1 })) {
+for await (const { name } of walk(cmps, { includeDirs: false, match: [/\.svg$/], maxDepth: 1 })) {
 	const start = performance.now();
 	const errors: string[] = [];
 
-	const svg = await Deno.readTextFile(`${cwd}/${name}`);
+	const svg = await Deno.readTextFile(`${cmps}/${name}`);
 	const png = await render(svg);
 
-	await Deno.writeFile(`${cwd}/build/${name.replace(".svg", ".png")}`, png)
+	await Deno.writeFile(`${build}/${name.replace(".svg", ".png")}`, png)
 		.catch((error) => errors.push(error instanceof Error ? error.message : String(error)));
 
 	if (name === "cmps.svg") {
-		await generate_favicon(`${cwd}/build/favicon.ico`, png)
+		await generate_favicon(`${build}/favicon.ico`, png)
 			.catch((error) => errors.push(error instanceof Error ? error.message : String(error)));
 	}
 
@@ -31,7 +31,3 @@ for await (const { name } of walk(cwd, { includeDirs: false, match: [/\.svg$/], 
 
 	console.info(status, cyan(name), "\t in", Math.ceil(performance.now() - start), "ms", "\t", errors.join(","));
 }
-
-await copy(`${cwd}/build`, new URL("../static", import.meta.url), {
-	overwrite: true,
-});
