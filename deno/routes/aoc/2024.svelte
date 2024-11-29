d9118cc7a28704beba35c7ac0f1042696df805b6
Max Duval <max.duval@hometrack.com>
2024-11-29 15:49:02 +0000

    refactor: organise folders better

---
+++ deno/routes/aoc/2024.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Layout from "../../components/aoc/Layout.svelte";
+
+	const year = 2024;
+	const days = /** @type {const} */ (
+		[
+			// all them days…
+		]
+	);
+</script>
+
+<Layout {days} {year}>
+	<p>
+		Check back soon… or have <a href="/aoc/2023">a look at 2023</a>
+	</p>
+</Layout>
