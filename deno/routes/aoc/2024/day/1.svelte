86a84feb0e2eb483500ec82ea87927ab39aced98
Max Duval <max.duval@hometrack.com>
2024-11-29 13:54:59 +0000

    feat: prep for AoC 2024

---
+++ deno/routes/aoc/2024/day/1.svelte
@@ -0,0 +1,35 @@
+<script>
+	let input = ``;
+
+
+</script>
+
+<textarea rows="7" bind:value={input}></textarea>
+
+
+
+<style>
+	li {
+		padding: 0.25rem;
+	}
+
+	span:not(.first, .last) {
+		opacity: 0.36;
+	}
+
+	.first,
+	.last {
+		font-weight: bold;
+	}
+	.first {
+		color: var(--blue);
+	}
+
+	.last {
+		color: var(--red);
+	}
+
+	.first.last {
+		color: var(--green);
+	}
+</style>
---
+++ deno/routes/aoc/day.svelte
@@ -0,0 +1,18 @@
+<script>
+	const year = 2024;
+	const days = /** @type {const} */ ([
+		// all them days
+		1]);
+</script>
+
+For the year {year}:
+
+<ul>
+	{#each days as day}
+		<li>
+			Day {String(day).replace("day/", "")} –
+			<a href="https://adventofcode.com/{}year}/{day}">the problem</a> and
+			<a href="/aoc/{year}/day/{day}">my solution</a>
+		</li>
+	{/each}
+</ul>
