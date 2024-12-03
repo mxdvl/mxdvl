b1c5f6efdd45a3eac3d5ba382d4d7ce91cc40009
Max Duval <max.duval@hometrack.com>
2024-12-03 07:57:52 +0000

    feat: 2024 day 3 part 1

---
+++ deno/components/aoc/Solution-2024-03.island.svelte
@@ -0,0 +1,52 @@
+<script>
+	let input = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
+
+	let part = "one";
+
+	const part_one_regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+
+	$: matches = [...input.matchAll(part_one_regex)].map(
+		([match, left, right]) => [match, left * right],
+	);
+
+
+</script>
+
+<textarea rows="7" bind:value={input}></textarea>
+
+<details open={part === "one"}>
+	<summary
+		>Part 1 – {matches.reduce(
+			(accumulator, [, multiplication]) => accumulator + multiplication,
+			0,
+		)}</summary
+	>
+
+	<ol>
+		{#each matches as [match, multiplication]}
+			<li>{match} = {multiplication}</li>
+		{/each}
+	</ol>
+</details>
+
+<details open={part === "two"}>
+	<summary>Part 2 – ???</summary>
+
+
+</details>
+
+<style>
+	.tolerance {
+		color: firebrick;
+		font-weight: bold;
+	}
+
+	.safe {
+		color: forestgreen;
+		font-weight: bold;
+	}
+
+	ol {
+		padding-left: 6ch;
+	}
+</style>
---
+++ deno/routes/aoc/2024/day/3.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Layout from "../../../../components/aoc/Layout.svelte";
+	import Solution from "../../../../components/aoc/Solution-2024-03.island.svelte";
+	import { year, days } from "../../2024.svelte";
+</script>
+
+<Layout {year} {days} day={3}>
+	<Solution />
+</Layout>
