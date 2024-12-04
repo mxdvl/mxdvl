b34eb61be6eee26fc409661716455ebdc2bcb02b
Max Duval <max.duval@hometrack.com>
2024-12-04 22:18:47 +0000

    chore: prep for day 5

---
+++ deno/components/aoc/Solution-2024-05.island.svelte
@@ -0,0 +1,29 @@
+<script>
+	let input = `TBC`;
+
+	let part = "one";
+</script>
+
+<textarea rows="7" bind:value={input}></textarea>
+
+<details open={part === "one"}>
+	<summary>Part 1 – ???</summary>
+</details>
+
+<details open={part === "two"}>
+	<summary>Part 2 – ???</summary>
+</details>
+
+<style>
+	.grid {
+		display: grid;
+	}
+
+	.green {
+		color: var(--green);
+	}
+
+	.red {
+		color: var(--red);
+	}
+</style>
---
+++ deno/routes/aoc/2024/day/5.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Layout from "../../../../components/aoc/Layout.svelte";
+	import Solution from "../../../../components/aoc/Solution-2024-05.island.svelte";
+	import { year, days } from "../../2024.svelte";
+</script>
+
+<Layout {year} {days} day={5}>
+	<Solution />
+</Layout>
