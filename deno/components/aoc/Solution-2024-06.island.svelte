bd512301cbd6bd083e5cd6eadf911818f275f45a
Max Duval <max.duval@hometrack.com>
2024-12-05 23:58:32 +0000

    chore: prep for day 6

---
+++ deno/components/aoc/Solution-2024-06.island.svelte
@@ -0,0 +1,51 @@
+<script>
+	let input = $state(`TBC`);
+
+	let part = $state("one");
+
+	let part_one = $derived(input.split("\n"))
+</script>
+
+<textarea rows="7" bind:value={input}></textarea>
+
+<details open={part === "one"}>
+	<summary
+		>Part 1 – {'???'}</summary
+	>
+
+	<ul>
+		{#each [] as _}
+			<li>
+			</li>
+		{/each}
+	</ul>
+
+
+</details>
+
+<details open={part === "two"}>
+	<summary
+		>Part 2 – {'???'}</summary
+	>
+
+	<ul>
+		{#each [] as _}
+			<li>
+			</li>
+		{/each}
+	</ul>
+</details>
+
+<style>
+	.grid {
+		display: grid;
+	}
+
+	.green {
+		color: var(--green);
+	}
+
+	.red {
+		color: var(--red);
+	}
+</style>
---
+++ deno/routes/aoc/2024/day/6.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Layout from "../../../../components/aoc/Layout.svelte";
+	import Solution from "../../../../components/aoc/Solution-2024-06.island.svelte";
+	import { year, days } from "../../2024.svelte";
+</script>
+
+<Layout {year} {days} day={6}>
+	<Solution />
+</Layout>
