c8207c73cbb72741590e486e11e0cf94f6110d05
Max Duval <max.duval@hometrack.com>
2024-12-10 06:47:20 +0000

    feat: small viz day 9

--- deno/components/aoc/Solution-2024-10.island.svelte
+++ deno/components/aoc/Solution-2024-10.island.svelte
@@ -15,7 +15,7 @@
 01329801
 10456732`);
 
-	let part = $state({ one: true, two: true });
+	let part = $state({ one: true, two: false });
 
 	let { map, width, height, bases } = $derived.by(() => {
 		const map = create_map(input);
@@ -59,22 +59,52 @@
 
 	let part_one = $derived.by(() => {
 		let total = 0;
+		/** @type {Map<Coordinates, Set<'N'|'W'|'S'|'W'>>} */
+		const connections = new Map();
 		for (const base of bases) {
 			let altitude = 0;
 			let edge = new Set([base]);
 			while (altitude < 9 && edge.size > 0) {
-				edge = new Set(
+				const next = new Set(
 					[...edge].flatMap((coord) =>
 						get_next(map, coord, altitude),
 					),
 				);
+				for (const e of edge) {
+					const { x: x1, y: y1 } = parse_coordinates(e);
+					for (const n of next) {
+						const from = connections.get(e) ?? new Set();
+						const to = connections.get(n) ?? new Set();
+						const { x: x2, y: y2 } = parse_coordinates(n);
+						const dx = x2 - x1;
+						const dy = y2 - y1;
+						if (dx === 0 && dy === 1) {
+							from.add("S");
+							to.add("N");
+						} else if (dx === 0 && dy === -1) {
+							from.add("N");
+							to.add("S");
+						} else if (dx === 1 && dy === 0) {
+							from.add("E");
+							to.add("W");
+						} else if (dx === -1 && dy === 0) {
+							from.add("W");
+							to.add("E");
+						}
+						connections.set(e, from);
+						connections.set(n, to);
+					}
+				}
+
+				edge = next;
 				altitude++;
 			}
 			if (altitude === 9) {
 				total += edge.size;
 			}
 		}
-		return { total };
+
+		return { total, connections };
 	});
 
 	let part_two = $derived.by(() => {
@@ -103,6 +133,53 @@
 	<summary>Part 1 – {part_one.total}</summary>
 
 	<div class="grid" style="--width:{width};">
+		{#each part_one.connections as [coordinates, connection]}
+			{@const { x, y } = parse_coordinates(coordinates)}
+			<div style="grid-area:{y + 1}/{x + 1}">
+				{#if connection.size === 4}
+					┼
+				{:else if connection.size === 3 && !connection.has("N")}
+					┬
+				{:else if connection.size === 3 && !connection.has("E")}
+					┤
+				{:else if connection.size === 3 && !connection.has("S")}
+					┴
+				{:else if connection.size === 3}
+					├
+				{:else if connection.size === 2 && connection.has("N") && connection.has("E")}
+					└
+				{:else if connection.size === 2 && connection.has("W") && connection.has("S")}
+					┐
+				{:else if connection.size === 2 && connection.has("S") && connection.has("E")}
+					┌
+				{:else if connection.size === 2 && connection.has("N") && connection.has("W")}
+					┘
+				{:else if connection.size === 2 && connection.has("N") && connection.has("S")}
+					│
+				{:else if connection.size === 2}
+					─
+				{:else if connection.size === 1}
+					&times;
+				{/if}
+			</div>
+		{/each}
+		{#each map as [coordinates, altitude]}
+			{@const { x, y } = parse_coordinates(coordinates)}
+			{@const green = altitude === 0}
+			{@const red = altitude === 9}
+			{#if red || green}
+				<div class:green class:red style="grid-area:{y + 1}/{x + 1};background:Canvas;">
+					{altitude}
+				</div>
+			{/if}
+		{/each}
+	</div>
+</details>
+
+<details bind:open={part.two}>
+	<summary>Part 2 – {part_two.total}</summary>
+
+	<div class="grid" style="--width:{width};">
 		{#each map as [coordinates, altitude]}
 			{@const { x, y } = parse_coordinates(coordinates)}
 			{@const green = altitude === 0}
@@ -112,12 +189,6 @@
 			</div>
 		{/each}
 	</div>
-</details>
-
-<details bind:open={part.two}>
-	<summary>Part 2 – {part_two.total}</summary>
-
-
 </details>
 
 <style>
