d7b20c9c3ef61813dc1eaa4b715db1a81b449548
Max Duval <max.duval@hometrack.com>
2024-12-10 05:36:46 +0000

    feat: 2024 day 10 part 1

--- deno/components/aoc/Solution-2024-10.island.svelte
+++ deno/components/aoc/Solution-2024-10.island.svelte
@@ -1,13 +1,82 @@
 <script>
-	import {} from "./helpers.js";
+	import {
+		create_map,
+		format_coordinates,
+		parse_coordinates,
+	} from "./helpers.js";
 	/** @typedef {`${number},${number}`} Coordinates */
 
-	let input = $state(``);
+	let input = $state(`89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`);
 
-	let part = $state("one");
+	let part = $state({ one: one, two: true });
+
+	let { map, width, height, bases } = $derived.by(() => {
+		const map = create_map(input);
+		let width = 0;
+		let height = 0;
+		/** @type {Set<Coordinates>} */
+		const bases = new Set();
+		for (const [coordinates, altitude] of map) {
+			const { x, y } = parse_coordinates(coordinates);
+			width = Math.max(width, x);
+			height = Math.max(height, y);
+			map.set(coordinates, parseInt(altitude, 10));
+			altitude === "0" && bases.add(coordinates);
+		}
+
+		return { map, width, height, bases };
+	});
+
+	/**
+	 *
+	 * @param {Map<Coordinates, number>} map
+	 * @param {Coordinates} coordinates
+	 * @param {number} altitude
+	 */
+	function get_next(map, coordinates, altitude) {
+		const { x, y } = parse_coordinates(coordinates);
+		return [
+			[0, -1], // N
+			[1, -0], // E
+			[-0, 1], // S
+			[-1, 0], // W
+		].flatMap(([dx, dy]) => {
+			const coordinates = format_coordinates({
+				x: x + dx,
+				y: y + dy,
+			});
+			return map.get(coordinates) === altitude + 1 ? [coordinates] : [];
+		});
+	}
 
 	let part_one = $derived.by(() => {
-		return "???";
+		let total = 0;
+		for (const base of bases) {
+			console.log("\n");
+			let altitude = 0;
+			let edge = new Set([base]);
+			while (altitude < 9 && edge.size > 0) {
+				edge = new Set(
+					[...edge].flatMap((coord) =>
+						get_next(map, coord, altitude),
+					),
+				);
+				// console.log(altitude, edge);
+				altitude++;
+			}
+			console.log(altitude, edge);
+			if (altitude === 9) {
+				total += edge.size;
+			}
+		}
+		return { total };
 	});
 
 	let part_two = $derived.by(() => {
@@ -17,11 +86,22 @@
 
 <textarea rows="10" bind:value={input}></textarea>
 
-<details open={part === "one"}>
-	<summary>Part 1 – {part_one}</summary>
+<details bind:open={part.one}>
+	<summary>Part 1 – {part_one.total}</summary>
+
+	<div class="grid" style="--width:{width};">
+		{#each map as [coordinates, altitude]}
+			{@const { x, y } = parse_coordinates(coordinates)}
+			{@const green = altitude === 0}
+			{@const red = altitude === 9}
+			<div class:green class:red style="grid-area:{y + 1}/{x + 1}">
+				{altitude}
+			</div>
+		{/each}
+	</div>
 </details>
 
-<details open={part === "two"}>
+<details bind:open={part.two}>
 	<summary>Part 2 – {part_two}</summary>
 </details>
 
