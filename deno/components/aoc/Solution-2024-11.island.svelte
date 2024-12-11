23001c00afdeb5e987461d165cc6859ba88de4c3
Max Duval <max.duval@hometrack.com>
2024-12-11 05:50:18 +0000

    feat: day 11 part 1

--- deno/components/aoc/Solution-2024-11.island.svelte
+++ deno/components/aoc/Solution-2024-11.island.svelte
@@ -7,12 +7,62 @@
 	} from "./helpers.js";
 	/** @typedef {`${number},${number}`} Coordinates */
 
-	let input = $state(``);
+
+	let input = $state(`125 17`);
 
 	let part = $state({ one: true, two: false });
 
+	let steps = $state(25);
+
+	let stones = $derived(
+		input.split(" ").flatMap((stone) => {
+			const digits = parseInt(stone, 10);
+			return Number.isInteger(digits) ? [BigInt(digits)] : [];
+		}),
+	);
+
 	let part_one = $derived.by(() => {
-		return "???";
+		let arrangement = [...stones];
+		let blinks = [[...arrangement]];
+		// console.log(...arrangement);
+		for (let step = 0; step < steps; step++) {
+			let index = 0;
+			while (index < arrangement.length) {
+				const stone = arrangement[index];
+				if (stone === undefined) break;
+				if (stone === 0n) {
+					arrangement[index] = 1n;
+					index += 1;
+					continue;
+				}
+				if (stone < 10n) {
+					arrangement[index] = stone * 2024n;
+					index += 1;
+					continue;
+				}
+				const digits = BigInt(String(stone).length);
+				// console.log(stone, String(stone), digits)
+				if (digits % 2n === 0n) {
+					const factor = 10n ** (digits / 2n);
+					const [left, right] = [stone / factor, stone % factor];
+
+					// console.log({ digits, stone, factor, left, right });
+					arrangement.splice(index, 1, left, right);
+					index += 2;
+					continue;
+				}
+
+				arrangement[index] = stone * 2024n;
+				index += 1;
+			}
+			// console.log(...arrangement);
+			// blinks.push([...arrangement]);
+			console.log(step, arrangement.length);
+		}
+
+		console.log(arrangement.length);
+
+		return { arrangement, blinks };
 	});
 
 	let part_two = $derived.by(() => {
@@ -23,14 +73,23 @@
 <textarea rows="10" bind:value={input}></textarea>
 
 <details bind:open={part.one}>
-	<summary>Part 1 – {part_one}</summary>
+	<summary>Part 1 – {part_one.arrangement.length}</summary>
+
+	<input type="range" bind:value={steps} max={25} />
+	{steps}
+	<!-- 185677 -->
+	<ul>
+		{#each part_one.blinks as blink}
+			<li>{blink.slice(0, 24).join(" ")}</li>
+		{/each}
+	</ul>
 </details>
 
 <details bind:open={part.two}>
 	<summary>Part 2 – {part_two}</summary>
 </details>
 
-<hr>
+<hr />
 
 <div class="grid">
 	<div class="red"></div>
