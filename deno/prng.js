aeb3e07760a8095394200e0fa0d1ad063259bbf8
Max Duval <max.duval@hometrack.com>
2025-01-09 12:00:05 +0000

    refactor: align to Rosetta code’s version
    https://rosettacode.org/wiki/Pseudo-random_numbers/PCG32

--- deno/prng.js
+++ deno/prng.js
@@ -1,4 +1,4 @@
-import { assertLess, assertEquals, assertGreaterOrEqual } from "jsr:@std/assert";
+import { assertEquals, assertGreaterOrEqual, assertLess } from "jsr:@std/assert";
 
 /**
  * Pseudo-random number generator of numbers in range [0, 1).
@@ -16,46 +16,51 @@ Deno.test({
 	name: "PNRG [0, 1)",
 	fn() {
 		assertEquals(Array.from(PRNG(42n).take(12)), [
-			0.7615582845173776,
-			0.41808728338219225,
-			0.4481155041139573,
-			0.2661335177253932,
-			0.9597071812022477,
-			0.4091600296087563,
-			0.7960081798955798,
-			0.8356900119688362,
-			0.48016405291855335,
-			0.996387166203931,
-			0.7091387419495732,
-			0.40304759168066084,
+			0.13170378981158137,
+			0.755355317145586,
+			0.5831400000024587,
+			0.21037689154036343,
+			0.9376081398222595,
+			0.6338424978312105,
+			0.7061422956176102,
+			0.06342564942315221,
+			0.2751847072504461,
+			0.004724327474832535,
+			0.18847966892644763,
+			0.5039901344571263,
 		]);
 
 		for (const seed of [42n, 120n, 987654321n, 0n]) {
 			for (const value of PRNG(seed).take(2 ** 18)) {
-				assertGreaterOrEqual(value, 0)
-				assertLess(value, 1)
+				assertGreaterOrEqual(value, 0);
+				assertLess(value, 1);
 			}
 		}
 	},
 });
 
-const MULTIPLIER = 6364136223846793005n;
-const INCREMENT = 1442695040888963407n;
-
 /**
  * [Permuted Congruential Generator](https://en.wikipedia.org/wiki/Permuted_congruential_generator) for a 32-bit unsigned integer
- * @param {bigint} [seed] defaults to `0x4d595df4d0f33173`.
+ * @param {bigint} seed defaults to `42n`.
+ * @param {bigint} sequence defaults to `0n`.
  */
-function* pcg32(seed = 5573589319906701683n) {
-	let state = seed + INCREMENT;
+function* pcg32(seed = 42n, sequence = 0n) {
+	const multiplier = 6364136223846793005n;
+	/** an even number, always */
+	const increment = BigInt.asUintN(64, (sequence << 1n) | 1n);
+	// let state = BigInt.asUintN(64, (seed + increment) * multiplier + increment);
+	let state = BigInt.asUintN(64, (increment + seed) * multiplier + increment);
 	while (true) {
-		state = BigInt.asUintN(64, state * MULTIPLIER + INCREMENT);
 		/** XOR-shifted */
 		const xsh = BigInt.asUintN(32, ((state >> 18n) ^ state) >> 27n);
 		const rotation = BigInt.asUintN(32, state >> 59n);
 		yield BigInt.asUintN(32, xsh >> rotation | xsh << (-rotation & 31n));
+		state = BigInt.asUintN(64, state * multiplier + increment);
 	}
 }
+
+//
+console.log([...pcg32(42n, 54n).take(5)]);
 
 Deno.test({
 	name: "PCG32",
@@ -63,12 +68,20 @@ Deno.test({
 		const prng = pcg32().take(6);
 
 		assertEquals(Array.from(prng), [
-			1150555626n,
-			1699080561n,
-			1140875202n,
-			3036938165n,
-			1448871516n,
-			4110472253n,
+			565663470n,
+			3244226384n,
+			2504567229n,
+			903561869n,
+			4026996297n,
+			2722332799n,
+		]);
+
+		assertEquals(Array.from(pcg32(42n, 54n).take(5)), [
+			2707161783n,
+			2068313097n,
+			3122475824n,
+			2211639955n,
+			3215226955n,
 		]);
 	},
 });
