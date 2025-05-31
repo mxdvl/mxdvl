e2530508d1d6390adcc0d71c411e4ab37bf32192
Max Duval <max.duval@hometrack.com>
2025-06-01 00:19:50 +0200

    feat: a JSX component QR codes

---
+++ deno/qr.tsx
@@ -0,0 +1,271 @@
+import { qr } from "npm:headless-qr@1";
+import { renderToString } from "npm:preact-render-to-string";
+
+if (import.meta.main) {
+	const server = Deno.serve(() =>
+		new Response(
+			renderToString(
+				<svg
+					version="1.1"
+					xmlns="http://www.w3.org/2000/svg"
+					width="1200"
+					height="1600"
+					viewBox="-10 -10 300 400"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<defs>
+						<symbol id="square">
+							<Square />
+						</symbol>
+						<style>
+							{`
+							text { font-family: system-ui; fill: green; }
+						`}
+						</style>
+					</defs>
+
+					<Token position={12} x={0} y={0} />
+					<Token position={34} x={30} y={0} />
+					<Token position={567} x={60} y={0} />
+					<Token position={890} x={90} y={0} />
+				</svg>,
+			),
+			{
+				headers: { "Content-Type": " image/svg+xml" },
+			},
+		)
+	);
+
+	await server.finished;
+}
+
+function Square() {
+	return (
+		<path
+			fill="none"
+			stroke="blue"
+			stroke-width="0.2"
+			transform="scale(0.5)"
+			d={[
+				"M0,0L2,2",
+				"M0,2L2,0",
+
+				"M0,0",
+				"A1,1 0,0,0 2,0",
+				"A1,1 0,0,0 2,2",
+				"A1,1 0,0,0 0,2",
+				"A1,1 0,0,0 0,0",
+			].join(" ")}
+		/>
+	);
+}
+
+function Token({ position, x = 0, y = 0 }: { position: number; x: number; y: number }) {
+	const input = `P${String(position).padStart(3, "0")}`;
+	const [first, second, third, fourth] = input;
+	return (
+		<g transform={`translate(${x} ${y})`}>
+			<path fill="none" stroke="red" d={["M0,0", "H28", "V53", "H0", "Z"].join(" ")} />
+			<g transform="translate(3 15)">
+				<Letter char={first ?? "P"} x={0} y={0} />
+				<Letter char={second ?? "0"} x={6} y={0} />
+				<Letter char={third ?? "0"} x={12} y={0} />
+				<Letter char={fourth ?? "0"} x={18} y={0} />
+			</g>
+			<QR input={input} x={4} y={28} />
+			{/* <text y={55} fontSize={6}>{input}</text> */}
+		</g>
+	);
+}
+
+function QR({ input, x = 0, y = 0 }: { input: string; x?: number; y?: number }) {
+	const positions = qr(input, { correction: "H" })
+		.flatMap((row, x) => row.flatMap((col, y) => col ? [{ x, y }] : []));
+
+	return (
+		<g transform={`translate(${x - 0.5} ${y - 0.5})`}>
+			{positions.map(({ x, y }) => (
+				<use
+					key={[x, y].join(",")}
+					x={x}
+					y={y}
+					href="#square"
+				/>
+			))}
+		</g>
+	);
+}
+
+function Letter({ char, x, y }: { char: string; x: number; y: number }) {
+	switch (char) {
+		case "P":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x},${y + 8}`,
+						"v-8",
+						"h2",
+						"a2,2 0 0 1 0,4",
+						"h-2",
+					].join(" ")}
+				/>
+			);
+		case "0":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x},${y + 2}`,
+						"a2,2 0 0 1 4,0",
+						"v4",
+						"a2,2 0 0 1 -4,0",
+						"Z",
+					].join(" ")}
+				/>
+			);
+		case "1":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x},${y + 2}`,
+						"a2,2 0 0 0 2,-2",
+						"v8",
+						"m-2,0",
+						"h4",
+					].join(" ")}
+				/>
+			);
+		case "2":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x},${y + 2}`,
+						"a2,2 0 1 1 2,2",
+						"a2,2 0 0 0 -2,2",
+						"v2",
+						"h4",
+					].join(" ")}
+				/>
+			);
+		case "3":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x},${y}`,
+						"h4",
+						"v1.5",
+						"a2,2 0 0 1 -2,2",
+						"m-2,0",
+						"h2",
+						"a2,2 0 0 1 2,2",
+						"v0.5",
+						"a2,2 0 0 1 -2,2",
+						"h-2",
+					].join(
+						" ",
+					)}
+				/>
+			);
+		case "4":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x + 3},${y + 8}`,
+						"v-8",
+						"l-3,4",
+						"h4",
+					].join(
+						" ",
+					)}
+				/>
+			);
+		case "5":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x + 4},${y}`,
+						"h-4",
+						"v4",
+						"h2",
+						"a2,2 0 0 1 0,4",
+						"h-2",
+					].join(
+						" ",
+					)}
+				/>
+			);
+		case "6":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x + 4},${y}`,
+						"h-2",
+						"a2,2 0 0 0 -2,2",
+						"v4",
+						"a2,2 0 0 0 4,0",
+						"v-1",
+						"a2,2 0 0 0 -4,0",
+					].join(
+						" ",
+					)}
+				/>
+			);
+		case "7":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x},${y}`,
+						"h4",
+						"v8",
+						"m-3,-4",
+						"h3",
+					].join(
+						" ",
+					)}
+				/>
+			);
+		case "8":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x},${y + 2}`,
+						"a2,2 0 0 1 4,0",
+						"a2,2 0 0 1 -4,0",
+						"m0,4",
+						"a2,2 0 0 1 4,0",
+						"a2,2 0 0 1 -4,0",
+					].join(
+						" ",
+					)}
+				/>
+			);
+		case "9":
+			return (
+				<path
+					stroke="blue"
+					d={[
+						`M${x + 4},${y + 2}`,
+						"a2,2 0 0 1 -4,0",
+						"a2,2 0 0 1 4,0",
+						"v4",
+						"a2,2 0 0 1 -4,0",
+					].join(
+						" ",
+					)}
+				/>
+			);
+		default:
+			return null;
+	}
+}
