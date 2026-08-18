31c9abc010d7ca5edd72132156d6c707e1087395
Max <hi@mxdvl.com>
2026-08-18 07:03:16 +0100

    add basic shape

---
+++ deno/components/Boussole.island.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	const now: Omit<Date, `set${string}`> = new Date()
+	let hours = $state(now.getHours())
+	let minutes = $state(now.getMinutes())
+</script>
+
+<svg viewBox="-60 -60 120 120" width="240" height="240">
+	<circle cx="0" cy="0" r="50" fill="none" stroke="black" />
+	<path d="" />
+</svg>
